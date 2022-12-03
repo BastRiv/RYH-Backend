4c7a9c39a23b143cb00297f5fbd41b1e2b5a1498
kevinzeladacl <iam@kevinzelada.cl>
2022-12-02 22:08:39 -0300

    admin

--- apps/directory/admin.py
+++ apps/directory/admin.py
@@ -5,23 +5,67 @@ from .models import *
 
 
 @admin.register(Property)
-class UserAdmin(admin.ModelAdmin):
+class PropertyAdmin(admin.ModelAdmin):
     list_display = (
-        'name',
+        'pk'
     )
-
+@admin.register(HistoryStatus)
+class HistoryStatusAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk'
+    )
 @admin.register(Service)
-class UserAdmin(admin.ModelAdmin):
+class ServiceAdmin(admin.ModelAdmin):
     list_display = (
-        'name',
+        'pk'
     )
-
+@admin.register(ServiceProperty)
+class ServicePropertyAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk'
+    )
 @admin.register(Reservation)
-class UserAdmin(admin.ModelAdmin):
+class ReservationAdmin(admin.ModelAdmin):
     list_display = (
-        'code',
-        'property'
+        'pk'
     )
-    
-
-
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk'
+    )
+@admin.register(ServicePropertyCheckInternal)
+class ServicePropertyCheckInternalAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk'
+    )
+@admin.register(ServicePropertyCheckIN)
+class ServicePropertyCheckINAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk'
+    )
+@admin.register(ServicePropertyCheckOUT)
+class ServicePropertyCheckOUTAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk'
+    )
+@admin.register(ServiceTransfer)
+class ServiceTransferAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk'
+    )
+@admin.register(ServiceTour)
+class ServiceTourAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk'
+    )
+@admin.register(ServiceExtraList)
+class ServiceExtraListAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk'
+    )
+@admin.register(ServiceExtra)
+class ServiceExtraAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk'
+    )
